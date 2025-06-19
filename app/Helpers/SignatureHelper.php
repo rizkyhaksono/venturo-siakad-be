68510b9b6a1de9f28b6f8d5028ce422531be6dbb
rizkyhaksono <mrizkyhaksono@gmail.com>
2025-06-19 15:55:30 +0700

    fix(middleware): undefined constant

--- app/Helpers/SignatureHelper.php
+++ app/Helpers/SignatureHelper.php
@@ -8,7 +8,7 @@ class SignatureHelper
   {
     $privateKey = file_get_contents(storage_path('auth/oauth-private.key'));
     $privateKeyResource = openssl_pkey_get_private($privateKey);
-    openssl_sign(json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_SORT_KEYS), $signature, $privateKeyResource, OPENSSL_ALGO_SHA256);
+    openssl_sign(json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES), $signature, $privateKeyResource, OPENSSL_ALGO_SHA256);
     openssl_free_key($privateKeyResource);
     return base64_encode($signature);
   }
@@ -17,7 +17,7 @@ class SignatureHelper
   {
     $publicKey = file_get_contents(storage_path('auth/oauth-public.key'));
     $publicKeyResource = openssl_pkey_get_public($publicKey);
-    $result = openssl_verify(json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_SORT_KEYS), base64_decode($signature), $publicKeyResource, OPENSSL_ALGO_SHA256);
+    $result = openssl_verify(json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES), base64_decode($signature), $publicKeyResource, OPENSSL_ALGO_SHA256);
     openssl_free_key($publicKeyResource);
     return $result === 1;
   }
