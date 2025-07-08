0e2e630f4643391219d0c00de966233700781edc
rizkyhaksono <mrizkyhaksono@gmail.com>
2025-07-09 00:40:48 +0700

    fix(request): import HttpResponseeException

--- app/Http/Requests/ClassRequest.php
+++ app/Http/Requests/ClassRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class ClassRequest extends FormRequest
 {
